8cd5a08fc961f7f86b28c669e0a0a760dd319dd1
Natcha Ranchan <natcha.ran@ku.th>
2025-04-17 21:43:32 +0700

    test for traffic controller

--- swagger_server/test/test_traffic_controller.py
+++ swagger_server/test/test_traffic_controller.py
@@ -18,10 +18,20 @@ class TestTrafficController(BaseTestCase):
         Get summary statistics for heart rate and traffic by traffic condition
         """
         response = self.client.open(
-            '/api/traffic/{isTrafficJam}'.format(isTrafficJam=True),
+            '/heart/v1/api/traffic/{is_traffic_jam}'.format(
+                is_traffic_jam=True),
             method='GET')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
+        response_data = json.loads(response.data.decode('utf-8'))
+        # check structure
+        self.assertIn('average_heartrate', response_data)
+        self.assertIn('average_speed', response_data)
+        self.assertIn('average_travel_time', response_data)
+        self.assertIn('max_heartrate', response_data)
+        self.assertIn('min_heartrate', response_data)
+        self.assertIn('number_of_records', response_data)
+        self.assertIsInstance(response_data, dict)
 
 
 if __name__ == '__main__':
