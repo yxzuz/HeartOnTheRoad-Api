b256970d554d588d6e6823e4112278ca6fa9fe9d
Natcha Ranchan <natcha.ran@ku.th>
2025-04-17 21:19:05 +0700

    add test caase for invalid trip_id

--- swagger_server/test/test_heart_rate_controller.py
+++ swagger_server/test/test_heart_rate_controller.py
@@ -55,19 +55,24 @@ class TestHeartRateController(BaseTestCase):
         ]
         self.assertEqual(response_json, my_json_list,
                          'Response does not match expected trip summary.')
-
-        # print(response_json)
-
+        
     def test_get_heart_rate_trip_invalid(self):
-        """Test case for invalid trip_id=999"""
+        """Test case for invalid trip_id=29"""
         response = self.client.open(
-            '/heart/v1/api/heartrate/trip/{trip_id}'.format(trip_id=999),
+            '/heart/v1/api/heartrate/trip/{trip_id}'.format(trip_id=29),
             method='GET')
+        self.assert400(response,'Expected 400 Bad Request for negative trip_id')
+        
+    def test_get_heart_rate_trip_with_string_id(self):
+        """Invalid string trip_id should return 500 (or 400 if you validate)"""
+        response = self.client.open('/heart/v1/api/heartrate/trip/a', method='GET')
+        self.assert500(response, 'Expected 500 Internal Server Error for invalid trip_id')
 
-        response_json = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(response_json, [])
+    def _get_heart_rate_trip_with_negative_id(self):
+        """Negative trip_id should return 400"""
+        response = self.client.open('/heart/v1/api/heartrate/trip/-2', method='GET')
+        self.assert400(response, 'Expected 400 Bad Request for negative trip_id')
 
-        # print(response.data.decode('utf-8'))
 
     def test_controller_get_heartrate_by_traffic(self):
         """Test case for controller_get_heartrate_by_traffic
@@ -75,11 +80,10 @@ class TestHeartRateController(BaseTestCase):
         Get timestamped heart rate data filtered by traffic condition
         """
         response = self.client.open(
-            '/heart/v1/api/heartrate/traffic/{isTrafficJam}'.format(
-                isTrafficJam=True),
+            '/heart/v1/api/heartrate/traffic/{is_traffic_jam}'.format(
+                is_traffic_jam=True),
             method='GET')
 
-        # print(44444,response.data.decode('utf-8'))
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
         # Parse the JSON response
--- swagger_server/test/test_location_controller.py
+++ swagger_server/test/test_location_controller.py
@@ -18,21 +18,47 @@ class TestLocationController(BaseTestCase):
         Get location data for a trip
         """
         response = self.client.open(
-            '/api/location/trip/{trip_id}'.format(trip_id=1.2),
+            '/heart/v1/api/location/trip/{trip_id}'.format(trip_id=1),
             method='GET')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
         
+        response2 = self.client.open(
+            '/heart/v1/api/location/trip/{trip_id}'.format(trip_id='1'),
+            method='GET')
+        self.assert200(response2,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_location_trip_invalid(self):
+        """Test case for invalid trip_id=29"""
+        response = self.client.open(
+            '/heart/v1/api/location/trip/{trip_id}'.format(trip_id=29),
+            method='GET')
+        self.assert400(response,'Expected 400 Bad Request for negative trip_id')
+        
+    def test_get_location_trip_with_string_id(self):
+        """Invalid string trip_id should return 500 (or 400 if you validate)"""
+        response = self.client.open('/heart/v1/api/location/trip/a', method='GET')
+        self.assert500(response, 'Expected 500 Internal Server Error for invalid trip_id')
+
+    def test_get_location_trip_with_negative_id(self):
+        """Negative trip_id should return 400"""
+        response = self.client.open('/heart/v1/api/location/trip/-2', method='GET')
+        self.assert400(response, 'Expected 400 Bad Request for negative trip_id')
+
     def test_controller_get_speed_by_traffic(self):
         """Test case for controller_get_speed_by_traffic
 
         Get current speed data filtered by traffic condition
         """
         response = self.client.open(
-            '/api/speed/traffic/{isTrafficJam}'.format(is_traffic_jam=true),
+            '/heart/v1/api/speed/traffic/{is_traffic_jam}'.format(
+                is_traffic_jam=True),
             method='GET')
+        response_json = json.loads(response.data.decode('utf-8'))   
         self.assert200(response,
-                        'Response body is : ' + response.data.decode('utf-8'))
+                       'Response body is : ' + response.data.decode('utf-8'))
+        self.assertIsInstance(response_json, list)
 
 
 if __name__ == '__main__':
